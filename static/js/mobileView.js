8189592818526a8d99be37b008ce17fb3fdff75a
unknown <miroslavb45@gmail.com>
2019-05-18 19:31:12 +0200

    Mobile view init

---
+++ static/js/mobileView.js
@@ -0,0 +1,44 @@
+function setPages(){
+    const height = screen.height;
+    const width = screen.width;
+
+    if(width <= 425){
+        let normalPages = document.getElementsByClassName("web");
+        [...normalPages].forEach(normalPage => {
+            normalPage.remove();
+            // normalPage.setAttribute("style","display: none;");
+            // normalPage.removeAttribute("data-anchor");
+        });
+
+        // let mobilePages = document.getElementsByClassName("mobile");
+        // [...mobilePages].forEach(mobilePage => {
+        //     let dataAnchorValue = mobilePage.getAttribute("_data-anchor");
+        //     mobilePage.setAttribute("data-anchor", dataAnchorValue);
+        //     mobilePage.removeAttribute("_data-anchor");
+            
+        // });
+
+       
+        // let coffefeMobile = document.getElementById("coffefeMobile");
+        // let dataAnchor = coffefeMobile.getAttribute("_data-anchor");
+        // coffefeMobile.setAttribute("data-anchor", dataAnchor);
+        // coffefeMobile.removeAttribute("_data-anchor");
+       
+        // let coffefe = document.getElementById("coffefe");
+        // coffefe.style.display = "none";
+        // coffefe.removeAttribute("data-anchor");
+
+
+    }else{
+        let mobilePages = document.getElementsByClassName("mobile");
+        [...mobilePages].forEach(mobilePage => {
+            mobilePage.remove();
+            // normalPage.setAttribute("style","display: none;");
+            // normalPage.removeAttribute("data-anchor");
+        });
+    }
+
+}
+
+
+
